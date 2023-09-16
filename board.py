c7c12c18088fbb65fe9b59ef83cc87050f5f606f
Matthew Levene <matthew.levene@hotmail.co.uk>
2023-09-17 00:21:30 +0100

    Draw the grid and add on click listeners

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
-import tkinter as tk
+from tkinter import ttk, Tk
+from functools import partial
 
 
 class Board:
@@ -6,13 +7,30 @@ class Board:
     def __init__(self):
         self._draw_board(3, 3)
 
-    def _draw_board(self, columns, rows):
+    def _update_board(self, row, column):
+        pass
+
+    def _draw_board(self, rows, columns):
         self.columns = columns
         self.rows = rows
 
-        window = tk.Tk()
+        window = Tk()
         window.title("Tic-Tac-Toe")
-        window.geometry("500x500+750+200")
+        window.geometry("500x300+750+200")
+
+        frame = ttk.Frame(window, padding=20)
+        frame.grid()
+
+        for row in range(rows):
+            for column in range(columns):
+                on_button_press = partial(self._update_board, row, column)
+                button = ttk.Button(
+                    frame,
+                    text="Row {}, Col {}".format(row, column),
+                    padding=35,
+                    command=on_button_press
+                )
+                button.grid(row=row, column=column)
 
         # Init the main looper and show the window
         window.mainloop()
