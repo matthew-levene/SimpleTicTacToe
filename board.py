106c8a17d2608c1aabbb357586b5143124249d47
Matthew Levene <matthew.levene@hotmail.co.uk>
2023-09-20 20:36:37 +0100

    Add functionality to allow user to select to be X or O and persist the option throughout the game

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, Tk
+from tkinter import ttk, Tk, StringVar
 from functools import partial
 
 
@@ -6,17 +6,34 @@ class Board:
 
     def __init__(self):
         self.round_count = 1
-        self._draw_board(3, 3)
+        self.player_marker = "x"
+
+        window = Tk()
+        window.title("Tic-Tac-Toe")
+        window.geometry("500x460+750+200")
+
+        self._draw_choice_input(window)
+        self._draw_board(window, 3, 3)
+
+        # Init the main looper and show the window
+        window.mainloop()
+
+    def _no_marker_selected(self):
+        return self.player_marker == "x"
 
     def _get_marker_style(self):
-        if self.round_count % 2:
+        print(self.round_count % 2 == 0)
+
+        if self._no_marker_selected():
+            self.player_marker = self.player_marker.upper()
+        if self.round_count % 2 == 0:
             style = ttk.Style()
             style.configure("RW.TButton", foreground="red", background="white")
-            return "X", "RW.TButton"
+            return "X" if self.player_marker == "X" else "O", "RW.TButton"
         else:
             style = ttk.Style()
             style.configure("BW.TButton", foreground="blue", background="white")
-            return "O", "BW.TButton"
+            return "O" if not self.player_marker == "O" else "X", "BW.TButton"
 
     def _update_board(self, frame, row, column):
         marker, style = self._get_marker_style()
@@ -31,14 +48,52 @@ class Board:
 
         self.round_count += 1
 
-    def _draw_board(self, rows, columns):
+    def _draw_choice_input(self, window):
+        marker_selection = StringVar()
+        on_radio_selected = partial(self._store_player_choice, marker_selection)
+
+        choice_label = ttk.Label(window, text="Do you want to play as X or O?", padding=15)
+        choice_label.grid()
+
+        choice_frame = ttk.Frame()
+
+        x_text = "X"
+        x_choice = ttk.Radiobutton(
+            choice_frame,
+            text=x_text,
+            variable=marker_selection,
+            value=x_text,
+            padding=10,
+            command=on_radio_selected
+        )
+        x_choice.grid(row=0, column=0)
+
+        o_text = "O"
+        o_choice = ttk.Radiobutton(
+            choice_frame,
+            text=o_text,
+            variable=marker_selection,
+            value=o_text,
+            padding=10,
+            command=on_radio_selected
+        )
+        o_choice.grid(row=0, column=1)
+
+        choice_frame.grid()
+
+    def _store_player_choice(self, marker_selection):
+        if self.player_marker != "O" and self.player_marker != "X":
+            if self._no_marker_selected():
+                self.player_marker = marker_selection.get()
+                print("Marker chosen for first time, setting as {}".format(self.player_marker))
+            else:
+                self.player_marker = marker_selection.get()
+                print("PLAYER CHOSEN MARKER {}".format(marker_selection.get()))
+
+    def _draw_board(self, window, rows, columns):
         self.columns = columns
         self.rows = rows
         self._board: list[list[str]] = [[] for _ in range(rows)]
-
-        window = Tk()
-        window.title("Tic-Tac-Toe")
-        window.geometry("500x320+750+200")
 
         frame = ttk.Frame(window, padding=20)
         frame.grid()
@@ -56,6 +111,3 @@ class Board:
                     command=on_button_press
                 )
                 button.grid(row=row, column=column)
-
-        # Init the main looper and show the window
-        window.mainloop()
